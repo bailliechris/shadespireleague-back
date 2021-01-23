633dec07071aeadb3d2103964877a7694d6663a3
Chris <bailliechristopher@gmail.com>
2021-01-23 22:21:16 +0000

    Added Read, Update and Delete for Posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,113 @@
+// Todo: 23/1/21
+// Test view specific posts route
+// Test deleting a post route
+
 const express = require('express');
 const router = express.Router();
 
-// Check Session
-const checkSession = require('../util/check')
-// Load User model
-const Post = require('../models/post');
+// Load route protection method
+const checkSession = require('../util/check');
 
+// Load Post and User models
+const Post = require('../models/post');
+//const User = require('../models/user');
+
+// Create a post
+// req.body to contain title, postedBy(mongoID), content
 router.post('/create', checkSession, (req, res) => {
     // Create post and saving
-        var post = new Post({
-            title: req.body.title,
-            postedBy: req.body.postedBy,
-            content: req.body.content
-        });
-        post.save().then(post => {
-            res.send(post);
-        }, (e) => {
-            res.status(400).send(e);
-        })
+    var post = new Post({
+        title: req.body.title,
+        postedBy: req.body.postedBy,
+        content: req.body.content
+    });
+    post.save().then(post => {
+        res.send(post);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+// Protected reading of posts
+router.post('/', checkSession, (req, res) => {
+    Post.find({postedBy: req.body.postedBy})
+    .then(post => {
+        if(post) {            
+            res.send(post);          
+        }
+        else {
+            res.send("No posts found")
+        } 
+
+    }).catch((e) => {     
+        res.status(400).send(e);  
+    });
+});
+
+// Get all posts
+router.get('/', (req, res) => {
+    Post.find({})
+    .then(post => {
+        if(post) {            
+            res.send(post);          
+        }
+        else {
+            res.send("No posts found")
+        } 
+
+    }).catch((e) => {     
+        res.status(400).send(e);  
+    });
+});
+
+
+// Update a Post
+// Post: postedBy, title, content
+router.post('/update', checkSession, (req, res) => {
+    Post.findOne({post:req.body.postid})
+    .then(post => {
+        if(post) {
+            if(req.body.title) {
+                post.title = req.body.title;
+            }
+
+            if(req.body.content) {
+                post.content = req.body.content;
+            }
+
+            post.save().then(post => {
+                res.send(post);
+            }, (e) => {
+                res.status(400).send(e);
+            })
+
+        }
+        else {
+            res.send("No such post");
+        }
+    })
+    .catch((e) => {      
+        // Error - send error code
+        res.status(400).send(e);    
+    });
+});
+
+// Remove a post - req.body to include postid - _id of post.
+router.post('/delete', checkSession, (req, res) => {
+    Post.deleteOne({post:req.body.postid})
+    .then(post => {
+        if(post) {
+            res.send("Post Removed");
+        }
+        else
+        {
+            res.send("No such post found");
+        }
+    })
+    .catch((e) => {      
+        // Error - send error code
+        res.status(400).send(e);    
+    });
 });
 
 module.exports = router;
